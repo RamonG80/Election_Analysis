bcccd00cfdbc882d4f5e409bf55b5c35d7b28142
Ramon A. Gamble <Ramon.Gamble@umusic.com>
2021-03-13 20:54:56 -0500

    Ramon adding the election audit code. FINALLY AGAIN.

---
+++ .vscode/ConditionalCode.py
@@ -0,0 +1,6 @@
+#count = 7
+#while count < 1:
+ #   print("Hello World")
+
+ counties_tuple = ("Arapahoe","Denver","Jefferson")
+ for i in range(len(counties_tuple)): print(counties_tuple[i])
---
+++ Python_practice.py
@@ -0,0 +1,32 @@
+#counties = ["Arapahoe","Denver","Jefferson"]
+
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+    for county in counties_tuple:
+        print(county)
+
+#for i in range(len(counties_tuple)):
+ #   print(counties_tuple[i])
+#if counties[1] == 'Denver':
+ #   print(counties[1])
+ #if counties[3] != 'Jefferson'
+ #print(counties[2])
+
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if "El Paso" in counties:
+ #   print("El Paso is in the list of counties.")
+#else:
+ #   print("El Paso is not the list of counties.")
+
+# if "Arapahoe" in counties and "El Paso" not in counties:
+ #  print("Only Arapahoe is in the list of counties.")
+#else:
+ #   print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+    #3.2.10 
+   # for county in counties:
+   # print(county)
+#numbers = [0, 1, 2, 3, 4]
+#for num in numbers:
+ #   print(num)
+#for num in range(5):
+ #   print(num)
